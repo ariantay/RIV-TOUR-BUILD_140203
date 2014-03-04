506512f72fb201450836ccb362e042bdd2443462
ariantay <ariantay@gmail.com>
2014-03-03 16:01:33 -0800

    fixed app issue by rebuilding

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -13,7 +13,7 @@ var app = {
     },
     gotoTourHome: function() {
          //window.alert("Map page is still loading.  Please be patient, your network might be unstable. For the meanwhile, please use the Statue List page instead");
-        if (typeof mapper === 'object' && typeof google === 'object' && typeof google.maps === 'object') {
+        if (typeof google === 'object' && typeof google.maps === 'object') {
             $.mobile.changePage('#tourpage_home');
         }else{
             window.alert("Map page is still loading.  Please be patient, your network might be unstable. For the meanwhile, please use the Statue List page instead");
@@ -124,7 +124,7 @@ var app = {
         globalLon = position.coords.longitude;
 		//update our map marker and radius
 		if (mapLoaded && typeof google === 'object' && typeof google.maps === 'object'){
-            console.log('calling google maps latlng and referencing mapper ' + globalLat + " " + globalLon);
+            console.log('calling google maps latlng and referencing mapper');
 			var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 			mapper.marker.setPosition(latlng);
 			mapper.circle.setCenter(latlng);
